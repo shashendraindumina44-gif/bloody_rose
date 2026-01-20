5dd71e7b927647a77af243c17d239811dbdd123e
shashendraindumina44-gif <shashendraindumina44@gmail.com>
2026-01-20 06:57:45 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    session_id: "PUT_SESSION_ID_HERE", // Session ID එකක් ගත්තොත් විතරක් මෙතනට දාන්න
+    bot_name: "Bloody Rose 🌹🩸",
+    owner_name: "Lord Indumina 🗣️",
+    owner_number: ["94763003966"], // ඔයාගේ WhatsApp අංකය (947...)
+    prefix: "."
+};
